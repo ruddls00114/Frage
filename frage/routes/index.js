33ee6252d0c99dcd6da2aba4daeffa24ee858b50
ruddls00114 <kkmm114@gmail.com>
2018-08-14 23:39:50 +0900

    ADD user folder

--- frage/routes/index.js
+++ frage/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/user',require('./user/user_index'))
+router.use('/board',require('./board/board_index'))
+
 module.exports = router;
