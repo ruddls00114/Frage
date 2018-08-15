c91f43e5b2c3aa202922fc5a737bb8057ca0fc5c
ruddls00114 <kkmm114@gmail.com>
2018-08-15 13:19:14 +0900

    modify GET reply

--- frage/routes/reply/reply.js
+++ frage/routes/reply/reply.js
@@ -7,31 +7,56 @@ const db = require('../../module/pool.js');
 const moment = require('moment');
 const upload = require('../../module/multer.js');
 
-
+//댓글 내용, 작성자 id, name, 날짜
 
 /*
-    마이페이지
+    댓글 가져오기 
  Method : Get
  */
-router.get('/',async (req, res, next) => {
-    const {user_idx} =req.query;
+router.get('/', async (req, res, next) => {
+    const { board_idx } = req.query;
 
-    // var frage_image = req.file ? req.file.location : null;
-    
-    let selectUserQuery = 
-    `
-    SELECT name, id, major, intro, image
-    FROM users
-    WHERE idx = ?
+    let userResult;
+    let selectUserQuery =
+        `
+    SELECT content,writedate, user_idx, users.name,
+    FROM replys
+    LEFT JOIN users ON replys.user_idx =users.idx
+    WHERE board_idx = ?
     `
     try {
-        let userResult = await db.Query(selectUserQuery,[user_idx]);
+        userResult = await db.Query(selectUserQuery, [board_idx]);
 
-        
+
     } catch (error) {
         return next(error);
     }
-    return res.r(userResult[0]);
+    return res.r(userResult);
+
+});
+
+/*
+    댓글 쓰기
+Method : Post
+*/
+router.post('/', async (req, res, next) => {
+    const { board_idx, user_idx, content } = req.body;
+
+    // var frage_image = req.file ? req.file.location : null;
+    let userResult;
+    let insertQuery =
+        `
+            INSERT INTO replys(board_idx,user_idx,content, writedate)
+            VALUES(?, ?, ?, ?)
+            `
+    try {
+        insertQuery = await db.Query(insertQuery, [board_idx, user_idx, content, moment().format('YYYY-MM-DD hh:mm')]);
+
+
+    } catch (error) {
+        return next(error);
+    }
+    return res.r();
 
 });
 
